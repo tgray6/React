65a0088001e9ffd7e08e65859d7bceaf65be3d1e
tgray6 <tylergrayx@gmail.com>
2024-03-13 00:30:26 -0400

    on backend

--- ReactPlayground/src/App.tsx
+++ ReactPlayground/src/App.tsx
@@ -1,8 +1,23 @@
-import FormWithRefs from "./Forms/Components/FormWithRefs";
-import FormWithUseState from "./Forms/Components/FormWithUseState";
-import FormWithReactHookForm from "./Forms/Components/FormWithReactHookForm";
-import ExpenseTrackerProject from "./ExpenseTracker/Components/ExpenseTrackerProject";
+// import FormWithRefs from "./Forms/Components/FormWithRefs";
+// import FormWithUseState from "./Forms/Components/FormWithUseState";
+// import FormWithReactHookForm from "./Forms/Components/FormWithReactHookForm";
+// import ExpenseTrackerProject from "./ExpenseTracker/Components/ExpenseTrackerProject";
+
+import { Input } from "antd";
+import { useEffect, useRef } from "react";
 
 export default function App() {
-  return <ExpenseTrackerProject />;
+  const inputRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = "React Playground";
+  }, []);
+
+  return <Input ref={inputRef} type="text" />;
 }
